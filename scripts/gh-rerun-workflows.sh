b6dd5e5fcdb292e474b00f8e65da4d89dd3f85e5
Jorge Niedbalski <jnr@metaklass.org>
2021-03-04 16:38:11 -0300

    added script to re-run workflows that are marked as skipped

---
+++ scripts/gh-rerun-workflows.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for f in $(curl  -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${REPOSITORY}/actions/runs | jq -r '.workflow_runs[] | select(.conclusion == "skipped") | .rerun_url'); do
+  curl --header "Authorization: token ${GITHUB_TOKEN}" $f;
+done
