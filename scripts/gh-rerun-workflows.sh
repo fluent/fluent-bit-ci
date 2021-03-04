3eb380c1880aa3769fce0770215f06e5ab43f46d
Jorge Niedbalski <jnr@metaklass.org>
2021-03-04 17:01:56 -0300

    added rerun-ok-to-test job
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- scripts/gh-rerun-workflows.sh
+++ scripts/gh-rerun-workflows.sh
@@ -3,7 +3,7 @@
 set -ex
 
 for f in $(curl  -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${REPOSITORY}/actions/runs | jq -r '.workflow_runs[] | select(.conclusion == "skipped") | .rerun_url'); do
-  curl --header "Authorization: token ${GITHUB_TOKEN}" $f
+  curl -s --header "Authorization: token ${GITHUB_TOKEN}" -X POST $f
 done
 
 exit 0
