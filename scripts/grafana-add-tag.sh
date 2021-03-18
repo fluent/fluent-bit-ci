0f5edd69a67cc8163fcd8b2983cdf3478168769e
Jorge Niedbalski <jnr@metaklass.org>
2021-03-18 17:56:21 -0300

    Update grafana-add-tag.sh

--- scripts/grafana-add-tag.sh
+++ scripts/grafana-add-tag.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 set -ex
+START_DATE=$(date +%s%N | cut -b1-13)
 
 curl -X POST https://fluentbit.grafana.net/api/annotations \
      -H "Content-Type: application/json" \
      -H "Authorization: Bearer ${GRAFANA_CLOUD_TOKEN}" \
      --data @- << EOF
      {
-       "time":$(date +%s%N | cut -b1-13),
+       "time":${START_DATE},
        "tags":["commit-master"],
        "text":"Commit: ${COMMIT_ID}, message: ${COMMIT_MESSAGE}"
      }
