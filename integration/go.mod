e4794b2e7c2ab9e7b281cfc11fb8b85e2124ec7f
Jorge Niedbalski <jnr@metaklass.org>
2021-05-24 09:19:37 -0400

    enable bigquery tests
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- integration/go.mod
+++ integration/go.mod
@@ -3,6 +3,7 @@ module github.com/calyptia/fluent-bit-ci/integration
 go 1.16
 
 require (
+	cloud.google.com/go/bigquery v1.0.1
 	github.com/flosch/pongo2/v4 v4.0.2
 	github.com/gruntwork-io/terratest v0.34.7
 	github.com/stretchr/testify v1.7.0
--- integration/tests/bigquery/test_dummy_input_bigquery_output.go
+++ integration/tests/bigquery/test_dummy_input_bigquery_output.go
@@ -1,30 +1,89 @@
+// +build integration
+
 package bigquery
 
 import (
+	bq "cloud.google.com/go/bigquery"
+	"context"
+	"fmt"
 	"github.com/calyptia/fluent-bit-ci/integration/tests"
+	"github.com/gruntwork-io/terratest/modules/retry"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"google.golang.org/api/iterator"
+	"os"
 	"time"
 )
 
 type Suite struct {
-	tests.BaseTestSuite
+	tests
 }
 
-const WaitInterval = 30 * time.Minute
+func queryBasic(projectID string, tableID string) ([]bq.value, error) {
+	ctx := context.Background()
+	client, err := bq.NewClient(ctx, projectID)
+	if err != nil {
+		return nil, fmt.Errorf("bigquery.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	q := client.Query(fmt.Sprintf("SELECT count(*) FROM `%s.testing_dataset.%s`", projectID, tableID))
+	q.Location = "US"
+	job, err := q.Run(ctx)
+	if err != nil {
+		return nil, err
+	}
+	status, err := job.Wait(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if err := status.Err(); err != nil {
+		return nil, err
+	}
+	it, err := job.Read(ctx)
+	for {
+		var row []bq.Value
+		err := it.Next(&row)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		return row, nil
+	}
+
+	return nil, fmt.Errorf("no results found")
+}
+
+func GetEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultVal
+}
+
+const DefaultMaxRetries = 5
+const DefaultRetryTimeout = 1 * time.Minute
 
 func (suite *Suite) TestDummyInputBigQueryOutput() {
+
 	cfg, _ := suite.RenderCfgFromTpl("dummy_input_bigquery_output", "", nil)
 	opts, _ := suite.GetTerraformOpts(cfg)
 
 	defer terraform.Destroy(suite.T(), opts)
 	terraform.InitAndApply(suite.T(), opts)
 
-	////pod, err := k8s.GetPodE(suite.T(), suite.K8sOptions, podName)
-	////suite.Nil(err)
-	//
-	//_, err = suite.RunKubectlExec(podName, "/bin/sh", "-c",
-	//	"rm -fr /data/test.log && /run_log_generator.py --log-size-in-bytes 1000 --log-rate 200000 --log-agent-input-type tail --tail-file-path /data/test.log > /dev/null 2> /dev/null &")
-	//suite.Nil(err)
+	retry.DoWithRetry(suite.T(), "Check if bigquery table has entries", tests.DefaultMaxRetries, tests.DefaultRetryTimeout, func() (string, error) {
+		var results []bq.result
+		var err error
+		if results, err = queryBasic(GetEnv("GCP_PROJECT_ID", ""), GetEnv("GCP_BQ_TABLE_ID", "")); err != nil {
+			return "", err
+		}
+		if len(results) <= 0 {
+			return "", fmt.Errorf("no found results")
+		}
 
-	time.Sleep(WaitInterval)
+		return "", nil
+	})
 }
