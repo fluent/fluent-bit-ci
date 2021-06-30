ebb6bccf49011754c3f40ec762cf85b400c3cd61
Phillip Whelan <phillip.whelan@timestamp.cl>
2021-06-30 17:48:07 -0400

    Add new integration test for Stackdriver output plugin.

---
+++ integration/tests/stackdriver/setup.tf
@@ -0,0 +1,62 @@
+provider "helm" {
+  kubernetes {
+    config_path = "client.config"
+  }
+}
+
+variable "gcp-sa-key" {
+  type = string
+}
+
+variable "prometheus-config" {
+  type = string
+}
+
+variable "fluent-bit-config" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+data "local_file" "fluent-bit-config" {
+  filename = basename(var.fluent-bit-config)
+}
+
+data "local_file" "prometheus-config" {
+  filename = basename(var.prometheus-config)
+}
+
+data "local_file" "gcp_sa_key" {
+  filename = basename(var.gcp-sa-key)
+}
+
+resource "kubernetes_secret" "service_account_data" {
+  metadata {
+    name      = "google-service-account-sa-key"
+    namespace = var.namespace
+  }
+  data = {
+    "google_service_credentials.json" = data.local_file.gcp_sa_key.content
+  }
+}
+
+resource "helm_release" "fluent-bit" {
+  name       = "fluent-bit"
+  namespace  = var.namespace
+  force_update = true
+  repository = "https://fluent.github.io/helm-charts"
+  chart      = "fluent-bit"
+  values = [data.local_file.fluent-bit-config.content]
+  depends_on = [kubernetes_secret.service_account_data]
+  wait = true
+}
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  namespace  = var.namespace
+  values = [data.local_file.prometheus-config.content]
+}
---
+++ integration/tests/stackdriver/test_dummy_input_stackdriver_output.go
@@ -0,0 +1,86 @@
+// +build integration
+
+package stackdriver
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/calyptia/fluent-bit-ci/integration/tests"
+	"github.com/gruntwork-io/terratest/modules/retry"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+
+	"cloud.google.com/go/logging"
+	"cloud.google.com/go/logging/logadmin"
+	"google.golang.org/api/iterator"
+)
+
+type Suite struct {
+	tests.BaseTestSuite
+}
+
+func getEntries(adminClient *logadmin.Client, projID string) ([]*logging.Entry, error) {
+	ctx := context.Background()
+
+	// [START logging_list_log_entries]
+	var entries []*logging.Entry
+	const name = "log-example"
+	lastHour := time.Now().Add(-1 * time.Hour).Format(time.RFC3339)
+
+	iter := adminClient.Entries(ctx,
+		// Only get entries from the "log-example" log within the last hour.
+		logadmin.Filter(fmt.Sprintf(`logName = "projects/%s/logs/%s" AND timestamp > "%s"`, projID, name, lastHour)),
+		// Get most recent entries first.
+		logadmin.NewestFirst(),
+	)
+
+	// Fetch the most recent 20 entries.
+	for len(entries) < 20 {
+		entry, err := iter.Next()
+		if err == iterator.Done {
+			return entries, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		entries = append(entries, entry)
+	}
+	return entries, nil
+	// [END logging_list_log_entries]
+}
+
+func (suite *Suite) TestDummyInputToStackdriverOutput() {
+	cfg, _ := suite.RenderCfgFromTpl("dummy_input_stackdriver_output", "values", nil)
+	opts, _ := suite.GetTerraformOpts(cfg)
+
+	defer terraform.Destroy(suite.T(), opts)
+	terraform.InitAndApply(suite.T(), opts)
+
+	ctx := context.Background()
+	cli, err := logging.NewClient(ctx, "integration-fluent-bit")
+	suite.Nil(err)
+	suite.NotNil(cli)
+
+	defer cli.Close()
+
+	admin, err := logadmin.NewClient(ctx, "integration-fluent-bit")
+	suite.Nil(err)
+	suite.NotNil(admin)
+
+	defer admin.Close()
+
+	retry.DoWithRetryInterface(suite.T(), "Check to see if we get any log entries from stackdriver", tests.DefaultMaxRetries, tests.DefaultRetryTimeout, func() (interface{}, error) {
+		logs, err := getEntries(admin, "integration-fluent-bit")
+		if err != nil {
+			return "", err
+		}
+		return logs, nil
+	})
+	//for, _ entry := range entries {
+	//fmt.Printf("Entry: %6s @%s: %v\n",
+	//	entry.Severity,
+	//	entry.Timestamp.Format(time.RFC3339),
+	//	entry.Payload)
+	//}
+}
