e2f9d3cbff7ea20059785c63256f3b97eea243cd
Phillip Whelan <phil@calyptia.com>
2021-07-08 16:07:58 -0400

    Add a function to wait for a certain number of results to appear for a splunk query.

--- integration/tests/splunk/test_dummy_input_splunk_output.go
+++ integration/tests/splunk/test_dummy_input_splunk_output.go
@@ -4,20 +4,84 @@ package splunk
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/calyptia/fluent-bit-ci/integration/tests"
+	"github.com/flosch/pongo2/v4"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/retry"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"strconv"
-	"time"
 )
 
 type Suite struct {
 	tests.BaseTestSuite
 }
 
+type result struct {
+	Raw     string `json:"_raw"`
+	LastRow bool   `json:"lastrow"`
+}
+
+type header struct {
+	Result result `json:"result"`
+}
+
+func (suite *Suite) waitCountResults(qry string, cnt int) int {
+	podName, _ := suite.GetPodNameByPrefix("splunk")
+	count, err := retry.DoWithRetryInterfaceE(suite.T(), fmt.Sprintf("Count results for: %s", qry), tests.DefaultMaxRetries, tests.DefaultRetryTimeout, func() (interface{}, error) {
+		output, err := suite.RunKubectlExec(podName, "bash", "-c",
+			fmt.Sprintf("curl -s -u admin:Admin123! -k https://localhost:8089/services/search/jobs/export -d output_mode=csv -d search='%s' | wc -l", qry))
+		if err != nil {
+			return "", err
+		}
+		if output == "" || output == "0\n" || output == "0" {
+			return "", fmt.Errorf("no results from splunk search")
+		}
+		elementsOnIndex, err := strconv.Atoi(output)
+		if err != nil {
+			return nil, err
+		}
+		if elementsOnIndex < cnt {
+			return nil, err
+		}
+		return elementsOnIndex, nil
+	})
+	suite.Nil(err)
+	return count.(int)
+}
+
+func (suite *Suite) countResults(qry string) int {
+	podName, _ := suite.GetPodNameByPrefix("splunk")
+	count, err := retry.DoWithRetryInterfaceE(suite.T(), fmt.Sprintf("Count results for: %s", qry), tests.DefaultMaxRetries, tests.DefaultRetryTimeout, func() (interface{}, error) {
+		output, err := suite.RunKubectlExec(podName, "bash", "-c",
+			fmt.Sprintf("curl -s -u admin:Admin123! -k https://localhost:8089/services/search/jobs/export -d output_mode=csv -d search='%s' | wc -l", qry))
+		if err != nil {
+			return "", err
+		}
+		if output == "" || output == "0\n" || output == "0" {
+			return "", fmt.Errorf("no results from splunk search")
+		}
+		elementsOnIndex, err := strconv.Atoi(output)
+		if err != nil {
+			return nil, err
+		}
+		return elementsOnIndex, nil
+	})
+
+	suite.Nil(err)
+	return count.(int)
+}
+
 func (suite *Suite) TestDummyInputToSplunkOutput() {
-	k8sDeployment, _ := suite.RenderCfgFromTpl("splunk-deployment", "k8s", nil)
+	random := fmt.Sprintf("%d", rand.Int())
+	testID := strings.Join([]string{"dummy", random}, "-")
+
+	k8sDeployment, _ := suite.RenderCfgFromTpl("splunk-deployment", "k8s", pongo2.Context{
+		"test_id": testID,
+	})
 
 	k8s.KubectlApply(suite.T(), suite.K8sOptions, k8sDeployment)
 	k8s.WaitUntilServiceAvailable(suite.T(), suite.K8sOptions, "splunk-master", 3, 1*time.Minute)
@@ -28,16 +92,9 @@ func (suite *Suite) TestDummyInputToSplunkOutput() {
 	defer terraform.Destroy(suite.T(), opts)
 	terraform.InitAndApply(suite.T(), opts)
 
-	podName, _ := suite.GetPodNameByPrefix("splunk")
-	elementsOnIndex := retry.DoWithRetry(suite.T(), "Check if search for testing string returns", tests.DefaultMaxRetries, tests.DefaultRetryTimeout, func() (string, error) {
-		output, err := suite.RunKubectlExec(podName, "bash", "-c", "curl -s -u admin:Admin123! -k https://localhost:8089/services/search/jobs/export -d output_mode=csv -d search='search testing' | wc -l")
-		if output == "0" || err != nil {
-			return "", fmt.Errorf("results from splunk index search<= 0")
-		}
-		return output, nil
-	})
-
-	elementsOnIndexConv, err := strconv.Atoi(elementsOnIndex)
-	suite.Nil(err)
-	suite.Greater(elementsOnIndexConv, 0)
+	// wait for both records to appear, both raw and JSON parsed
+	suite.waitCountResults(testID, 2)
+	// and then see if we only get one event that can be searched as if
+	// it was parsed as JSON
+	suite.waitCountResults(fmt.Sprintf("test_id=%s", testID), 1)
 }
