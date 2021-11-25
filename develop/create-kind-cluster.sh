8bbb02597be3f4fbe9a3c49f130d7e4a9509afd8
Patrick Stephens <pat@calyptia.com>
2021-11-25 16:32:13 +0000

    Initial attempt

---
+++ develop/create-kind-cluster.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Copyright 2021 Calyptia, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file  except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the  License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -eu
+# Simple script to provision a Kubernetes cluster using KIND: https://kind.sigs.k8s.io/
+
+# Override with a different name if you want
+CLUSTER_NAME=${CLUSTER_NAME:-kind}
+# The fluent bit image under test
+FLUENT_BIT_IMAGE=${FLUENT_BIT_IMAGE:-fluent/fluent-bit:1.8.10}
+
+if [[ "${INSTALL_KIND:-no}" == "yes" ]]; then
+    rm -f ./kind
+    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+    chmod a+x ./kind
+    sudo mv ./kind /usr/local/bin/kind
+fi
+
+# Delete the old cluster (if it exists)
+kind delete cluster --name="${CLUSTER_NAME}"
+
+# Create KIND cluster with 3 worker nodes
+kind create cluster --name="${CLUSTER_NAME}" --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+- role: worker
+EOF
+
+# Preload the cluster with the Fluent Bit image on every node
+kind load docker-image "${FLUENT_BIT_IMAGE}" --name="${CLUSTER_NAME}"
+
+echo "Cluster created successfully"
+echo "Use 'kind load docker-image <image tag>' to push images into the nodes, otherwise they will be pulled"
---
+++ develop/deploy-elastic.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+# Copyright 2021 Calyptia, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file  except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the  License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -eu
+# Simple script to deploy Elastic to a Kubernetes cluster with context already set
+
+ES_NAMESPACE=${ES_NAMESPACE:-elastic}
+CLUSTER_NAME=${CLUSTER_NAME:-kind}
+
+cat << EOF | kubectl apply -f -
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: $ES_NAMESPACE
+---
+kind: Service
+apiVersion: v1
+metadata:
+  name: elasticsearch
+  namespace: $ES_NAMESPACE
+  labels:
+    app: elasticsearch
+spec:
+  selector:
+    app: elasticsearch
+  clusterIP: None
+  ports:
+    - port: 9200
+      name: rest
+    - port: 9300
+      name: inter-node
+---
+apiVersion: apps/v1
+kind: StatefulSet
+metadata:
+  name: es-cluster
+  namespace: $ES_NAMESPACE
+spec:
+  serviceName: elasticsearch
+  replicas: 1
+  selector:
+    matchLabels:
+      app: elasticsearch
+  template:
+    metadata:
+      labels:
+        app: elasticsearch
+    spec:
+      containers:
+      - name: elasticsearch
+        image: docker.elastic.co/elasticsearch/elasticsearch:7.2.0
+        resources:
+            limits:
+              cpu: 1000m
+            requests:
+              cpu: 100m
+        ports:
+        - containerPort: 9200
+          name: rest
+          protocol: TCP
+        - containerPort: 9300
+          name: inter-node
+          protocol: TCP
+        env:
+          - name: cluster.name
+            value: k8s-logs
+          - name: node.name
+            valueFrom:
+              fieldRef:
+                fieldPath: metadata.name
+          - name: discovery.seed_hosts
+            value: "es-cluster-0.elasticsearch"
+          - name: cluster.initial_master_nodes
+            value: "es-cluster-0"
+          - name: ES_JAVA_OPTS
+            value: "-Xms512m -Xmx512m"
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: kibana
+  namespace: $ES_NAMESPACE
+  labels:
+    app: kibana
+spec:
+  ports:
+  - port: 5601
+  selector:
+    app: kibana
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: kibana
+  namespace: $ES_NAMESPACE
+  labels:
+    app: kibana
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: kibana
+  template:
+    metadata:
+      labels:
+        app: kibana
+    spec:
+      containers:
+      - name: kibana
+        image: docker.elastic.co/kibana/kibana:7.2.0
+        resources:
+          limits:
+            cpu: 1000m
+          requests:
+            cpu: 100m
+        env:
+          - name: ELASTICSEARCH_URL
+            value: http://elasticsearch:9200
+        ports:
+---
+EOF
+
+echo "Elasticsearch deployed to elasticsearch.$ES_NAMESPACE"
